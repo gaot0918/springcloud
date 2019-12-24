4daa1d47b9ea56de3debea49900f45b659dda60a
gaot <gaotaoe@qq.com>
2019-12-24 22:07:27 +0800

    eureka-server init

---
+++ eureka-server/src/main/java/com/gaot/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.gaot.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author gaot
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
